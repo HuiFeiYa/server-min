3278f8bad2164204653b1dcb3e8c6c08385fb285
guanwanxiao <18257132004@163.com>
2019-07-07 22:00:55 +0800

    change

--- router/index.js
+++ router/index.js
@@ -1,48 +1,48 @@
 const Router = require('koa-router')
-const {
-  login,
-  test,
-  todayStep,
-  getPhoto,
-  photoUpload
-} = require('./controller')
-const path = require('path')
-const auth = require('../middlewares/index')
+// const {
+//   login,
+//   test,
+//   todayStep,
+//   getPhoto,
+//   photoUpload
+// } = require('./controller')
+// const path = require('path')
+// const auth = require('../middlewares/index')
 const router = new Router()
 
-const multer = require('koa-multer')
+// const multer = require('koa-multer')
 
-const storage = multer.diskStorage({
-  destination:
-    'uploads/' +
-    new Date().getFullYear() +
-    (new Date().getMonth() + 1) +
-    new Date().getDate(),
-  filename: function(req, file, cb) {
-    let fileFormat = file.name.split('.')
-    console.log('fileFormat', fileFormat)
+// const storage = multer.diskStorage({
+//   destination:
+//     'uploads/' +
+//     new Date().getFullYear() +
+//     (new Date().getMonth() + 1) +
+//     new Date().getDate(),
+//   filename: function(req, file, cb) {
+//     let fileFormat = file.name.split('.')
+//     console.log('fileFormat', fileFormat)
 
-    let filename =
-      file.fieldname +
-      '-' +
-      Date.now() +
-      '.' +
-      fileFormat[fileFormat.length - 1]
-    cb(null, filename)
-  }
-})
+//     let filename =
+//       file.fieldname +
+//       '-' +
+//       Date.now() +
+//       '.' +
+//       fileFormat[fileFormat.length - 1]
+//     cb(null, filename)
+//   }
+// })
 
-const upload = multer({ storage })
+// const upload = multer({ storage })
 
 module.exports = app => {
   // 经过auth的筛选，如果过期会直接返回，否则进入login就是登陆
-  router.post('/login', auth, login)
-  router.post('/test', test)
-  router.post('/step/today', todayStep)
+  // router.post('/login', auth, login)
+  // router.post('/test', test)
+  // router.post('/step/today', todayStep)
   // router.post('/photo', getPhoto)
   router.post('/photo', async ctx => {
     ctx.body = '<h1>hello word</h1>'
   })
-  router.post('/upload', upload.single('avatar'), photoUpload)
+  // router.post('/upload', upload.single('avatar'), photoUpload)
   app.use(router.routes())
 }
