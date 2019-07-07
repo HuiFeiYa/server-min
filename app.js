ac95942a0aaadd84621c29faeaa1bc0e24c4c4f2
guanwanxiao <18257132004@163.com>
2019-07-07 22:27:44 +0800

    ch

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = new Koa()
 const koaBody = require('koa-body')
 // const router = require('./router')
 const bodyParser = require('koa-bodyparser')
-// const cors = require('./utils/cors')
+const cors = require('./utils/cors')
 // const static = require('koa-static')
 // const path = require('path')
 const Router = require('koa-router')
@@ -18,7 +18,7 @@ const rou = new Router()
 // app.use(static(path.join(__dirname)))
 
 // app.use(bodyParser())
-// app.use(cors())
+app.use(cors())
 rou.post('/photo', async ctx => {
   ctx.body = '<h1>hello word</h1>'
 })
