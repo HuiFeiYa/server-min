f5e5140b4098b09cdbb821c8eca6a2ca6f7f9209
guanwanxiao <18257132004@163.com>
2019-07-08 15:08:10 +0800

    单独设置文件

--- app.js
+++ app.js
@@ -1,49 +1,12 @@
 const Koa = require('koa')
 const app = new Koa()
-const koaBody = require('koa-body')
+
 const router = require('./router')
 const bodyParser = require('koa-bodyparser')
 const cors = require('./utils/cors')
 const static = require('koa-static')
 const path = require('path')
-const {
-  getUploadDirName,
-  checkDirExist,
-  getUploadFileExt,
-  getUploadFileName
-} = require('./utils/file')
 
-app.use(
-  koaBody({
-    multipart: true, // 支持文件上传
-    encoding: 'gzip',
-    formidable: {
-      // uploadDir: path.join(__dirname, 'uploads'), // 设置文件上传目录
-      keepExtensions: true, // 保持文件的后缀
-      maxFieldsSize: 2 * 1024 * 1024, // 文件上传大小
-      onFileBegin: (name, file) => {
-        // 文件上传前的设置
-        // 获取文件后缀
-        // const ext = getUploadFileExt(file.name)
-        // 最终要保存到的文件夹目录
-        // const dir = path.join(__dirname, `uploads/${getUploadDirName()}`)
-        // 检查文件夹是否存在如果不存在则新建文件夹
-        // checkDirExist(dir)
-        // 重新覆盖 file.path 属性
-        const filename = file.name
-          .split('.')
-          .slice(2)
-          .join('.')
-        console.log('file-----', filename)
-        // 设置最终文件路径的位置，前面没有加决定路径默认拼接文件夹根路径
-        file.path = 'uploads/' + filename
-      },
-      onError: err => {
-        console.log(err)
-      }
-    }
-  })
-)
 // app.use(static(path.join(__dirname)))
 app.use(bodyParser())
 app.use(cors())
--- router/index.js
+++ router/index.js
@@ -11,7 +11,7 @@ const path = require('path')
 const auth = require('../middlewares/index')
 const router = new Router()
 const { fillZero } = require('../utils/date')
-
+const koaBody = require('koa-body')
 const multer = require('koa-multer')
 
 const storage = multer.diskStorage({
@@ -42,7 +42,40 @@ module.exports = app => {
   router.post('/word', async ctx => {
     ctx.body = '<h1>hello word</h1>'
   })
-  router.post('/upload', upload.single('avatar'), photoUpload)
+  router.post(
+    '/upload',
+    koaBody({
+      multipart: true, // 支持文件上传
+      encoding: 'gzip',
+      formidable: {
+        // uploadDir: path.join(__dirname, 'uploads'), // 设置文件上传目录
+        keepExtensions: true, // 保持文件的后缀
+        maxFieldsSize: 2 * 1024 * 1024, // 文件上传大小
+        onFileBegin: (name, file) => {
+          // 文件上传前的设置
+          // 获取文件后缀
+          // const ext = getUploadFileExt(file.name)
+          // 最终要保存到的文件夹目录
+          // const dir = path.join(__dirname, `uploads/${getUploadDirName()}`)
+          // 检查文件夹是否存在如果不存在则新建文件夹
+          // checkDirExist(dir)
+          // 重新覆盖 file.path 属性
+          const filename = file.name
+            .split('.')
+            .slice(2)
+            .join('.')
+          console.log('file-----', filename)
+          // 设置最终文件路径的位置，前面没有加决定路径默认拼接文件夹根路径
+          file.path = 'uploads/' + filename
+        },
+        onError: err => {
+          console.log(err)
+        }
+      }
+    }),
+    upload.single('avatar'),
+    photoUpload
+  )
   router.post('/phot-add', photoInsert)
   app.use(router.routes())
 }
