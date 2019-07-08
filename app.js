581b0992d857ea874e343594e4a89a1715f4b778
guanwanxiao <18257132004@163.com>
2019-07-08 10:55:04 +0800

    koabody

--- app.js
+++ app.js
@@ -21,28 +21,28 @@ router(app)
 app.use(
   koaBody({
     multipart: true, // 支持文件上传
-    encoding: 'gzip',
-    formidable: {
-      uploadDir: path.join(__dirname, 'uploads'), // 设置文件上传目录
-      keepExtensions: true, // 保持文件的后缀
-      maxFieldsSize: 2 * 1024 * 1024, // 文件上传大小
-      // onFileBegin: (name, file) => {
-      // 文件上传前的设置
-      // 获取文件后缀
-      // const ext = getUploadFileExt(file.name)
-      // // 最终要保存到的文件夹目录
-      // const dir = path.join(__dirname, `uploads/${getUploadDirName()}`)
-      // // 检查文件夹是否存在如果不存在则新建文件夹
-      // checkDirExist(dir)
-      // 重新覆盖 file.path 属性
-      //   console.log('file', file)
+    encoding: 'gzip'
+    // formidable: {
+    // uploadDir: path.join(__dirname, 'uploads'), // 设置文件上传目录
+    // keepExtensions: true, // 保持文件的后缀
+    // maxFieldsSize: 2 * 1024 * 1024, // 文件上传大小
+    // onFileBegin: (name, file) => {
+    // 文件上传前的设置
+    // 获取文件后缀
+    // const ext = getUploadFileExt(file.name)
+    // // 最终要保存到的文件夹目录
+    // const dir = path.join(__dirname, `uploads/${getUploadDirName()}`)
+    // // 检查文件夹是否存在如果不存在则新建文件夹
+    // checkDirExist(dir)
+    // 重新覆盖 file.path 属性
+    //   console.log('file', file)
 
-      //   file.path = 'my123.png'
-      // },
-      onError: err => {
-        console.log(err)
-      }
-    }
+    //   file.path = 'my123.png'
+    // },
+    // onError: err => {
+    //   console.log(err)
+    // }
+    // }
   })
 )
 app.listen(8083, () => {
