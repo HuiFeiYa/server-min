e43db6638b50dbac33d80a1eed61fd9b91b208ff
guanwanxiao <18257132004@163.com>
2019-07-08 09:59:41 +0800

    filename

--- router/index.js
+++ router/index.js
@@ -6,8 +6,8 @@ const {
   getPhoto,
   photoUpload
 } = require('./controller')
-// const path = require('path')
-// const auth = require('../middlewares/index')
+const path = require('path')
+const auth = require('../middlewares/index')
 const router = new Router()
 
 const multer = require('koa-multer')
@@ -17,16 +17,19 @@ const storage = multer.diskStorage({
     'uploads/' +
     new Date().getFullYear() +
     (new Date().getMonth() + 1) +
-    new Date().getDate()
+    new Date().getDate(),
+  filename: function(req, file, cb) {
+    console.log('file', file)
+  }
 })
 
 const upload = multer({ storage })
 
 module.exports = app => {
   // 经过auth的筛选，如果过期会直接返回，否则进入login就是登陆
-  // router.post('/login', auth, login)
-  // router.post('/test', test)
-  // router.post('/step/today', todayStep)
+  router.post('/login', auth, login)
+  router.post('/test', test)
+  router.post('/step/today', todayStep)
   router.post('/photo', getPhoto)
   router.post('/word', async ctx => {
     ctx.body = '<h1>hello word</h1>'
